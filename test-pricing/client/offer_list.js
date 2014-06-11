4a28904bba1f4fe2d48bd7a0e31ef5c56ad8c70b
Lionel Chanson <lionel@soreia.com>
2014-06-11 16:54:36 +0200

    offers data

---
+++ test-pricing/client/offer_list.js
@@ -0,0 +1,13 @@
+var offersData = [
+    {
+        name: "Early Bird",
+        price: 600
+    },
+    {
+        name: "Normal",
+        price: 1000
+    }
+];
+Template.offersList.helpers({
+    offers: offersData
+});
