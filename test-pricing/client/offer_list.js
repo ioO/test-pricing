c1c53758b4e8a76090a4067b358c3442cae6c1ec
Lionel Chanson <lionel@soreia.com>
2014-06-13 14:42:32 +0200

    compute line total and grand total

--- test-pricing/client/offer_list.js
+++ test-pricing/client/offer_list.js
@@ -11,3 +11,12 @@ var offersData = [
 Template.offerList.helpers({
     offers: offersData
 });
+
+Template.offerList.events({
+    'change select': function(event){
+        var target = event.currentTarget.id;
+        var multipliant = $("#" + target + " option:selected").val();
+        document.getElementById("sum_" + target).innerHTML = target * multipliant;
+        document.getElementById("total").innerHTML = parseInt(document.getElementById("sum_600").innerHTML) + parseInt(document.getElementById("sum_1000").innerHTML);
+    }
+});
